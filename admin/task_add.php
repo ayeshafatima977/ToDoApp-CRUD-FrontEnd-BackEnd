7645540d18a52706532333193b0bdbbb82ca5204
Ayesha Fatima <ayeshafatima977@gmail.com>
2020-10-16 12:05:33 -0600

    Add the complete and remove buttons to the task list;

--- admin/task_add.php
+++ admin/task_add.php
@@ -4,19 +4,20 @@ $categories_select_options = null;
 $message = null;
 $task_list = null;
 
-// Query for Selection of Category
-$categories_sql = "SELECT CategoryID, CategoryName FROM taskCategory";
-
 // Creating a connection and testing if connection is established or not
 $connection = new MySQLi(HOST, USER, PASSWORD, DATABASE);
 if ($connection->connect_errno) {
     die('Connection failed: ' . $connection->connect_error);
 }
+
+// Query for Selection of Category
+$categories_sql = "SELECT CategoryID, CategoryName FROM taskCategory";
+
 if (!$categories_result = $connection->query($categories_sql)) {
     echo "Something went wrong with the categories query";
     exit();
 }
-
+//To generate the Category from data base
 if ($categories_result->num_rows > 0) {
     while ($categories = $categories_result->fetch_assoc()) {
         $categories_select_options .= sprintf('
@@ -38,7 +39,7 @@ if (!$result = $connection->query($task_list_sql)) {
 
 // To check if there are any empty records/rows
 if (0 === $result->num_rows) {
-    $task_list = '<tr><td colspan="4">There are no Active Things To Do</td></tr>';
+    $task_list = '<tr><td colspan="5">There are no Active Things To Do</td></tr>';
 } else {
     while ($row = $result->fetch_assoc()) {
         $task_list .= sprintf('
@@ -46,49 +47,25 @@ if (0 === $result->num_rows) {
             <td>%d</td>
             <td>%s</td>
             <td>%s</td>
-            <td><a href="task_edit.php?task_id=%d">Edit</a></td>
+            <td>%s</td>
+            <td><a href="task_complete.php?task_id=%d">Complete</a></td>
+            <td><a href="task_delete.php?task_id=%d">Remove</a></td>
         </tr>
         ',
             $row['TaskID'],
             $row['TaskName'],
+            $row['StartDate'],
             $row['EndDate'],
+            $row['TaskID'],
             $row['TaskID']
         );
 
     }
 }
 
-if ($_POST) {
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
-    //Sanitize Input fields using PREPARE statement
-    // Add task to the list Here ? indicate Placeholders for the text
-    if ($insert = $connection->prepare("INSERT INTO task(TaskID, CategoryID, TaskName, TaskTimeMinutes, StartDate,EndDate)
-
-    VALUES(NULL, ?, ?, ?, ?, ?)")) {
-        //Since Task id is NULL and auto incremented it is not accounted here; i is integer for CategoryID;s is string for task Name;
-        //d is for double for TaskTime Minutes;s is string for start date; and s is string for end date
-
-        if ($insert->bind_param("isdss", $_POST['task_category'], $_POST['task_name'], $_POST['task_time'], $_POST['start_date'], $_POST['end_date'])) {
-            if ($insert->execute()) {
-                $message = "You have added " . $_POST['task_name'] . " to the database";
-            } else {
-                exit("There was a problem with the execute");
-            }
-        } else {
-            exit("There was a problem with the bind_param");
-        }
-    } else {
-        exit("There was a problem with the prepare statement");
-    }
-    $insert->close();
-}
 $connection->close();
 
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -121,27 +98,19 @@ $connection->close();
             <?php echo $categories_select_options; ?>
         </select>
         <input type="submit" value="Add new task">
-
-
+        <input type="submit" name="reset" value="Reset" />
         <h2>Things To Do</h2>
         <table>
             <tr>
                 <th>Task ID</th>
                 <th>Task Name</th>
+                <th>Start Date</th>
                 <th>End Date</th>
                 <th>Actions</th>
             </tr>
-
             <?php echo $task_list; ?>
 
         </table>
-
-
-
-
-
-
-
     </form>
 
 </body>
